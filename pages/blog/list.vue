e4b7d86394343c0616e588c8c544bbcb19b38317
Weerapat <weerapatsu@gmail.com>
2018-09-17 15:13:51 +0700

    Install dependencies

---
+++ pages/blog/list.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Hi Posts</h1>
+
+    {{ articles }}
+  </div>
+</template>
+
+<script>
+  export default {
+    async asyncData ({ app, store, params }) {
+      return { articles: await app.$wp.posts().perPage(10) }
+    }
+  }
+</script>
